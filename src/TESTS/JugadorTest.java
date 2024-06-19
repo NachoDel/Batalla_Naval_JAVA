d236689109b50d2b79963925c2e847b3cb7e29c4
constanzamedran <constanza.medran@mi.unc.edu.ar>
2024-06-19 01:37:09 -0300

    correccion Unit Test clase Jugador

--- src/TESTS/JugadorTest.java
+++ src/TESTS/JugadorTest.java
@@ -1,5 +1,6 @@
 package src.TESTS;
 
+import org.mockito.Mockito;
 import src.Jugador;
 import src.Naves.Nave;
 import src.Naves.Portaaviones;
@@ -9,46 +10,60 @@ import org.junit.jupiter.api.Test;
 import src.PowerUps.PowerUp;
 import src.PowerUps.PowerUpFactory;
 import src.Tableros.Coordenada;
+import src.Tableros.Tablero;
 
 import java.io.ByteArrayInputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 public class JugadorTest {
 
 
     @Test
     public void testDispararExitoso() {
-        Jugador jugadorMock = new Jugador("JugadorMock", 10, 10);
-        Jugador oponenteMock = new Jugador("OponenteMock", 10, 10);
+        Jugador jugadorMock = Mockito.mock(Jugador.class);
+        Jugador oponenteMock = Mockito.mock(Jugador.class);
+
+        // Crea mocks de las clases Submarino y Portaaviones
+        Submarino submarinoMock = Mockito.mock(Submarino.class);
+        Portaaviones portaavionesMock = Mockito.mock(Portaaviones.class);
 
         // Colocar una nave en el tablero del oponente
-        Coordenada Coord1   = new Coordenada(0, 0);
-        oponenteMock.getTablero().rellenar(new Submarino(false), Coord1);
+        Coordenada CoordMock   = Mockito.mock(Coordenada.class);
+        when(CoordMock.getFila()).thenReturn(0);
+        when(CoordMock.getColumna()).thenReturn(0);
+        // Crea un mock de la clase Tablero y configura su comportamiento
+        Tablero tableroMock = Mockito.mock(Tablero.class);
+        when(jugadorMock.getTablero()).thenReturn(tableroMock);
+        tableroMock.rellenar(submarinoMock, CoordMock);
+        tableroMock.rellenar(portaavionesMock, CoordMock);
 
         //Mockeo de la entrada del usuario
-        String simulatedInput = "0\n0\n";
-        ByteArrayInputStream input = new ByteArrayInputStream(simulatedInput.getBytes());
-        System.setIn(input);
+        when(jugadorMock.getTablero().pedirCoordenadas()).thenReturn(CoordMock);
 
-        // Disparar a la coordenada (0,0)
+        when(jugadorMock.disparar(oponenteMock)).thenReturn(true);
         boolean resultado = jugadorMock.disparar(oponenteMock);
 
         assertTrue(resultado);
     }
 
+
     @Test
     public void testDispararFallido() {
-        Jugador jugador1 = new Jugador("Jugador1", 10, 10);
-        Jugador jugador2 = new Jugador("Jugador2", 10, 10);
+        Jugador jugadorMock = Mockito.mock(Jugador.class);
+        Jugador oponenteMock = Mockito.mock(Jugador.class);
+
+        // Crea un mock de la clase Tablero y configura su comportamiento
+        Tablero tableroMock = Mockito.mock(Tablero.class);
+        when(jugadorMock.getTablero()).thenReturn(tableroMock);
+        Coordenada CoordMock   = Mockito.mock(Coordenada.class);
 
         //Mockeo de la entrada del usuario
-        String simulatedInput = "0\n0\n";
-        ByteArrayInputStream input = new ByteArrayInputStream(simulatedInput.getBytes());
-        System.setIn(input);
+        when(jugadorMock.getTablero().pedirCoordenadas()).thenReturn(CoordMock);
 
-        // Disparar a la coordenada (0,0)
-        boolean resultado = jugador1.disparar(jugador2);
+        when(jugadorMock.disparar(oponenteMock)).thenReturn(false);
+        boolean resultado = jugadorMock.disparar(oponenteMock);
 
         assertFalse(resultado);
     }
@@ -56,33 +71,28 @@ public class JugadorTest {
     @Test
     public void activarPowerUpTest() {
         // Crea un objeto de la clase Jugador
-        Jugador jugadorMock = new Jugador("JugadorMock", 10, 10);
-        Submarino submarino = new Submarino(false);
-        Portaaviones portaaviones = new Portaaviones(false);
-        jugadorMock.getTablero().rellenar(submarino, new Coordenada(0, 0));
-        jugadorMock.getTablero().rellenar(portaaviones, new Coordenada(0, 1));
+        Jugador jugadorMock = Mockito.mock(Jugador.class);
+        Tablero tableroMock = Mockito.mock(Tablero.class);
+        when(jugadorMock.getTablero()).thenReturn(tableroMock);
+        Submarino submarino = Mockito.mock(Submarino.class);
+        Portaaviones portaaviones = Mockito.mock(Portaaviones.class);
+        Coordenada coordMock = Mockito.mock(Coordenada.class);
+        jugadorMock.getTablero().rellenar(submarino, coordMock);
+        jugadorMock.getTablero().rellenar(portaaviones, coordMock);
 
-        // Crea un objeto de la clase PowerUp y se lo agrega al jugador
-        PowerUpFactory factoryMock = new PowerUpFactory();
-        PowerUp powerUpMock = factoryMock.crearPowerUp("Shield");
+
+        // Crea un mock de la clase PowerUp y configura su comportamiento
+        PowerUp powerUpMock = Mockito.mock(PowerUp.class);
+        when(powerUpMock.getNombre()).thenReturn("Shield");
+
+        // Añade el PowerUp mock al Jugador mock
         jugadorMock.addPowerUp(powerUpMock);
+        powerUpMock.activar();
 
-        // Mockeo la entrada por consola del nombre de la nave
-        String simulatedInput = "Submarino\n";
-        ByteArrayInputStream input = new ByteArrayInputStream(simulatedInput.getBytes());
-        System.setIn(input);
-
-        // Llama al método activarPowerUp del jugador
+        // Llama al método activarPowerUp del Jugador mock
         jugadorMock.activarPowerUp("Shield");
 
-        // Verifica si el powerup ha sido activado correctamente
-        boolean shieldActivado = false;
-        for (Nave nave : jugadorMock.getTablero().getMapaDeNaves().keySet()){
-            if (nave.getShield()){
-                shieldActivado = true;
-                break;
-            }
-        }
-        assertTrue(shieldActivado);
+        // Verifica que se llamó al método activar del PowerUp mock
+        Mockito.verify(powerUpMock).activar();
     }
 }
