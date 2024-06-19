e5760b78a2043166dcf983cae5198705fd03a5ad
ignaciorivarola <ignacio.rivarola@mi.unc.edu.ar>
2024-06-19 02:03:52 -0300

    check

--- src/TESTS/DoubleShotPUTest.java
+++ src/TESTS/DoubleShotPUTest.java
@@ -8,33 +8,37 @@ import src.Naves.Submarino;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
-@Test
-public void activarTest() {
-    // Mockeo el jugador y el submarino
-    Jugador jugadorMock = mock(Jugador.class);
-    Submarino submarinoMock = mock(Submarino.class);
-    Coordenada coordMock = mock(Coordenada.class);
+public class DoubleShotPUTest {
 
-    // Creato la instancia de DoubleShotPU
-    DoubleShotPU doubleShotPU = new DoubleShotPU();
+    @Test
+    public void activarTest() {
+        // Mockeo el jugador y el submarino
+        Jugador jugadorMock = mock(Jugador.class);
+        Submarino submarinoMock = mock(Submarino.class);
+        Coordenada coordMock = mock(Coordenada.class);
 
-    // Seteando el comportamiento del mock
-    when(jugadorMock.getTablero().getNavesConVida()).thenReturn(1);
-    when(jugadorMock.getTablero().getNave(coordMock)).thenReturn(submarinoMock);
+        // Creato la instancia de DoubleShotPU
+        DoubleShotPU doubleShotPU = new DoubleShotPU();
 
-    // Activacion del power up con mock
-    doubleShotPU.activar(jugadorMock, coordMock);
+        // Seteando el comportamiento del mock
+        when(jugadorMock.getTablero().getNavesConVida()).thenReturn(1);
+        when(jugadorMock.getTablero().getNave(coordMock)).thenReturn(submarinoMock);
 
-    // Verifico disparo
-    verify(jugadorMock.getTablero(), times(2)).recibirDisparo(coordMock);
+        // Activacion del power up con mock
+        doubleShotPU.activar(jugadorMock, coordMock);
 
-    // Verifico que el método getNavesConVida haya sido llamado una vez
-    verify(jugadorMock.getTablero(), times(1)).getNavesConVida();
+        // Verifico disparo
+        verify(jugadorMock.getTablero(), times(2)).recibirDisparo(coordMock);
 
-    // Verifico que el método getNave haya sido llamado una vez con la coordenada mockeada
-    verify(jugadorMock.getTablero(), times(1)).getNave(coordMock);
+        // Verifico que el método getNavesConVida haya sido llamado una vez
+        verify(jugadorMock.getTablero(), times(1)).getNavesConVida();
 
-    // Asserts
-    assertEquals(1, jugadorMock.getTablero().getNavesConVida());
-    assertEquals(submarinoMock, jugadorMock.getTablero().getNave(coordMock));
+        // Verifico que el método getNave haya sido llamado una vez con la coordenada mockeada
+        verify(jugadorMock.getTablero(), times(1)).getNave(coordMock);
+
+        // Asserts
+        assertEquals(1, jugadorMock.getTablero().getNavesConVida());
+        assertEquals(submarinoMock, jugadorMock.getTablero().getNave(coordMock));
+    }
 }
+
