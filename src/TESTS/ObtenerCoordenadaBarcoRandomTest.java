21d0a7df6be83d923af6a3b17010a225905fa102
ignaciorivarola <ignacio.rivarola@mi.unc.edu.ar>
2024-06-19 00:43:29 -0300

    TESET OBTENERCOORDRAND

---
+++ src/TESTS/ObtenerCoordenadaBarcoRandomTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+package src.TESTS;
+
+
+
+public class ObtenerCoordenadaBarcoRandomTest {
+
+    @Test
+    public void testObtenerCoordenadaBarcoRandom() {
+        // Crea mock de  ObtenerCoordenadaBarcoRandom
+        ObtenerCoordenadaBarcoRandom obtenerCoordenadaBarcoRandomMock = Mockito.mock(ObtenerCoordenadaBarcoRandom.class);
+
+        // Define esperado
+        int expectedX = 5;
+        int expectedY = 3;
+
+        // sEteo de comportamiento
+        when(obtenerCoordenadaBarcoRandomMock.obtenerCoordenadaX()).thenReturn(expectedX);
+        when(obtenerCoordenadaBarcoRandomMock.obtenerCoordenadaY()).thenReturn(expectedY);
+
+        // Llama prueba
+        int actualX = obtenerCoordenadaBarcoRandomMock.obtenerCoordenadaX();
+        int actualY = obtenerCoordenadaBarcoRandomMock.obtenerCoordenadaY();
+
+        // Prueba
+        assertEquals(expectedX, actualX);
+        assertEquals(expectedY, actualY);
+    }
+}
