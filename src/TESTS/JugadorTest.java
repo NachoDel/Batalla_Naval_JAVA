eff313242c7cc81d34902c376034ea6c5114d7ef
constanzamedran <constanza.medran@mi.unc.edu.ar>
2024-06-18 22:51:45 -0300

    implementacion Unit Test del metodo Disparar de la clase Jugador

--- src/TESTS/JugadorTest.java
+++ src/TESTS/JugadorTest.java
@@ -4,21 +4,31 @@ import src.Jugador;
 import src.Naves.Submarino;
 
 import org.junit.jupiter.api.Test;
+import src.Tableros.Coordenada;
+
+import java.io.ByteArrayInputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class JugadorTest {
 
-    /*
+
     @Test
     public void testDispararExitoso() {
-        Jugador jugador1 = new Jugador("Jugador1", 10, 10);
-        Jugador jugador2 = new Jugador("Jugador2", 10, 10);
+        Jugador jugadorMock = new Jugador("JugadorMock", 10, 10);
+        Jugador oponenteMock = new Jugador("OponenteMock", 10, 10);
 
-        // Colocar una nave en el tablero del jugador2
-        jugador2.getTablero().colocarNaveParaTest(new Submarino());
+        // Colocar una nave en el tablero del oponente
+        Coordenada Coord1   = new Coordenada(0, 0);
+        oponenteMock.getTablero().rellenar(new Submarino(false), Coord1);
+
+        //Mockeo de la entrada del usuario
+        String simulatedInput = "0\n0\n";
+        ByteArrayInputStream input = new ByteArrayInputStream(simulatedInput.getBytes());
+        System.setIn(input);
 
         // Disparar a la coordenada (0,0)
-        boolean resultado = jugador1.dispararParaTest(jugador2, 1);
+        boolean resultado = jugadorMock.disparar(oponenteMock);
 
         assertTrue(resultado);
     }
@@ -28,9 +38,14 @@ public class JugadorTest {
         Jugador jugador1 = new Jugador("Jugador1", 10, 10);
         Jugador jugador2 = new Jugador("Jugador2", 10, 10);
 
+        //Mockeo de la entrada del usuario
+        String simulatedInput = "0\n0\n";
+        ByteArrayInputStream input = new ByteArrayInputStream(simulatedInput.getBytes());
+        System.setIn(input);
+        
         // Disparar a la coordenada (0,0)
-        boolean resultado = jugador1.dispararParaTest(jugador2, 1);
+        boolean resultado = jugador1.disparar(jugador2);
 
         assertFalse(resultado);
-    }*/
+    }
 }
