f91b55960f327a371be669dd5162bd17e1c05e6a
ignaciorivarola <ignacio.rivarola@mi.unc.edu.ar>
2024-06-18 23:10:01 -0300

    SHIELDPU y modidf DSPU

--- src/TESTS/DoubleShotPUTest.java
+++ src/TESTS/DoubleShotPUTest.java
@@ -8,26 +8,33 @@ import src.Naves.Submarino;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
-public class DoubleShotPUTest {
+@Test
+public void activarTest() {
+    // Mockeo el jugador y el submarino
+    Jugador jugadorMock = mock(Jugador.class);
+    Submarino submarinoMock = mock(Submarino.class);
+    Coordenada coordMock = mock(Coordenada.class);
 
-    @Test
-    public void activarTest() {
-        // Mockeo el jugador y el submarino
-        Jugador jugadorMock = mock(Jugador.class);
-        Submarino submarinoMock = mock(Submarino.class);
-        Coordenada coordMock = mock(Coordenada.class);
+    // Creato la instancia de DoubleShotPU
+    DoubleShotPU doubleShotPU = new DoubleShotPU();
 
-        // Creato la instancia de DoubleShotPU
-        DoubleShotPU doubleShotPU = new DoubleShotPU();
+    // Seteando el comportamiento del mock
+    when(jugadorMock.getTablero().getNavesConVida()).thenReturn(1);
+    when(jugadorMock.getTablero().getNave(coordMock)).thenReturn(submarinoMock);
 
-        // Seteando el comportamiento del mock
-        when(jugadorMock.getTablero().getNavesConVida()).thenReturn(1);
-        when(jugadorMock.getTablero().getNave(coordMock)).thenReturn(submarinoMock);
+    // Activacion del power up con mock
+    doubleShotPU.activar(jugadorMock, coordMock);
 
-        // Activacion del power up con mock
-        doubleShotPU.activar(jugadorMock, coordMock);
+    // Verifico disparo
+    verify(jugadorMock.getTablero(), times(2)).recibirDisparo(coordMock);
 
-        // Verifico disparo
-        verify(jugadorMock.getTablero(), times(2)).recibirDisparo(coordMock);
-    }
+    // Verifico que el método getNavesConVida haya sido llamado una vez
+    verify(jugadorMock.getTablero(), times(1)).getNavesConVida();
+
+    // Verifico que el método getNave haya sido llamado una vez con la coordenada mockeada
+    verify(jugadorMock.getTablero(), times(1)).getNave(coordMock);
+
+    // Asserts
+    assertEquals(1, jugadorMock.getTablero().getNavesConVida());
+    assertEquals(submarinoMock, jugadorMock.getTablero().getNave(coordMock));
 }
---
+++ src/TESTS/ShieldPUTest.java
@@ -0,0 +1,40 @@
+package src.TESTS;
+
+import src.Jugador;
+import src.PowerUps.ShieldPU;
+import src.Tableros.Coordenada;
+import src.Naves.Submarino;
+
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+public class ShieldPUTest {
+
+    @Test
+    public void activarTest() {
+        // Mockeo el jugador y el submarino
+        Jugador jugadorMock = mock(Jugador.class);
+        Submarino submarinoMock = mock(Submarino.class);
+        Coordenada coordMock = mock(Coordenada.class);
+
+        // Creato la instancia de ShieldPU
+        ShieldPU shieldPU = new ShieldPU();
+
+        // Seteando el comportamiento del mock
+        when(jugadorMock.getTablero().getNavesConVida()).thenReturn(1);
+        when(jugadorMock.getTablero().getNave(coordMock)).thenReturn(submarinoMock);
+
+        // Activacion del power up con mock
+        shieldPU.activar(jugadorMock, coordMock);
+
+        // Verifico que el método getNavesConVida haya sido llamado una vez
+        verify(jugadorMock.getTablero(), times(1)).getNavesConVida();
+
+        // Verifico que el método getNave haya sido llamado una vez con la coordenada mockeada
+        verify(jugadorMock.getTablero(), times(1)).getNave(coordMock);
+
+        // Asserts
+        assertEquals(1, jugadorMock.getTablero().getNavesConVida());
+        assertEquals(submarinoMock, jugadorMock.getTablero().getNave(coordMock));
+    }
+}
